bd51388bd24e010b12ea59681da2a24eb5f48d81
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-08-16 16:40:37 -0400

    set database url to depend on node_env

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: '*' }));
 
 // Connection to the database and error handling
-const url = process.env.DB_URI || 'mongodb://127.0.0.1:27017/issueTrackerDB';
+const url =
+  process.env.NODE_ENV === 'production'
+    ? process.env.DB_URI
+    : 'mongodb://127.0.0.1:27017/issueTrackerDB';
+
 mongoose
   .connect(url, {
     useNewUrlParser: true,
