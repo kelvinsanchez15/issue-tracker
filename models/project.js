d472a7fa7ba9100d7f832184a72a38ce29cdbc73
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-08-17 07:24:01 -0400

    delete unwanted comment

--- models/project.js
+++ models/project.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// Schema setup
 const Project = mongoose.model('Project', {
   name: { type: String, required: true, trim: true },
   issues: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Issue' }],
